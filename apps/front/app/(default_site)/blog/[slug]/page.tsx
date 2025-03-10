548c8265182a2ac232334fb06740bd01789465a0
Erasmo Perniola <eperniola@litebox.ai>
2025-03-10 16:22:55 +0000

    feat: added header for blog

---
+++ apps/front/app/(default_site)/blog/[slug]/page.tsx
@@ -0,0 +1,26 @@
+import { Suspense } from 'react';
+import { redirect } from 'next/navigation'
+// import MainMobile from '@/components/blogSlug/mobile/Main';
+// import MainDesktop from '@/components/blogSlug/desktop/Main';
+import { getArticle } from '@/backend/blog/actions';
+
+interface PageProps {
+    params: Promise<{ slug: string }>;
+}
+
+export default async function Page({ params }: PageProps) {
+    const { slug } = await params;
+    const article = await getArticle(slug);
+    if(!Object.keys(article).includes('id')){
+        redirect('/page-not-found');
+    }
+
+    return (
+        <main>
+            <Suspense>
+                {/* <MainDesktop article={article} />
+                <MainMobile article={article} /> */}
+            </Suspense>
+        </main>
+    );
+}
