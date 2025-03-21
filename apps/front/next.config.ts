c08dc1fb74a92d364a821eb41a905a3ea5d2e8b8
eperniola <eperniola@litebox.ai>
2025-03-21 12:16:55 -0300

    fix: reduced cache time

--- apps/front/next.config.ts
+++ apps/front/next.config.ts
@@ -3,11 +3,18 @@ import withBundleAnalyzer from '@next/bundle-analyzer';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['127.0.0.1', 'localhost', 'strapi.ai-nekko.litebox.dev'],
+    domains: ['127.0.0.1', 'localhost', 'strapi.ai-foundry.litebox.dev'],
     formats: ['image/avif', 'image/webp'],
-    minimumCacheTTL: 86400,
+    minimumCacheTTL: 3600,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'strapi.ai-foundry.litebox.dev',
+        pathname: '/uploads/**',
+      }
+    ]
   },
   compress: true,
   swcMinify: true,
@@ -26,12 +33,21 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
+            value: 'public, max-age=3600, stale-while-revalidate=60'
           },
         ],
       },
+      {
+        source: '/service-worker.js',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate'
+          }
+        ]
+      }
     ];
-  }
+  },
 };
 
 export default process.env.ANALYZE ? withBundleAnalyzer({ enabled: true })(nextConfig) : nextConfig;
