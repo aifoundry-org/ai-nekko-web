f96e5de0d889eeffbc3bfcc02782364e17798d9c
Roman Shaposhnik <roman@nekko.ai>
2026-01-29 02:17:13 -0800

    Getting page layout to barebone basics

--- apps/front/app/(default_site)/page.tsx
+++ apps/front/app/(default_site)/page.tsx
@@ -1,9 +1,9 @@
 import Hero from '@/components/home/hero/main'
 import PaperDiscussions from '@/components/home/paperDiscussions/main'
-import AiAsASystem from '@/components/home/aiAsASystem/main'
-import Marquee from '@/components/home/marquee/main'
+// import AiAsASystem from '@/components/home/aiAsASystem/main'
+// import Marquee from '@/components/home/marquee/main'
 import TheTeam from '@/components/home/theTeam/main'
-import Faqs from '@/components/home/faqs/main'
+// import Faqs from '@/components/home/faqs/main'
 
 export default function Page() {
   return (
